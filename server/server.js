055c30a6993cb67409fb188ae1efc14c769b9a00
Fauzia Shafi <fauziashafi12@gmail.com>
2022-03-10 09:33:26 -0800

    Adding starter code

--- server/server.js
+++ server/server.js
@@ -1,42 +1,61 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, ".env") });
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
 
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
+const { authMiddleware } = require("./util/auth");
 
 const PORT = process.env.PORT || 3001;
-const app = express();
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+startServer(typeDefs, resolvers);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+async function startServer(typeDefs, resolvers) {
+  try {
+    // Wait for db connection
+    await new Promise((resolve) => db.once("open", resolve));
+
+    // create apollo server
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      context: authMiddleware,
+    });
+    await server.start();
+
+    // create express app
+    const app = express();
+    server.applyMiddleware({
+      app,
+      path: "/graphql",
+    });
+
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+
+    if (process.env.NODE_ENV === "production") {
+      // Handle requests for client assets
+      app.use(express.static(path.join(__dirname, "../client/build")));
+
+      // Respond with react client for all other requests. This route should be
+      // the last route added to the express app.
+      app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../client/build/index.html"));
+      });
+    }
+
+    // start listening for requests
+    await new Promise((resolve) => app.listen({ port: PORT }, resolve));
+    console.log(
+      `🚀 Apollo Server ready at http://localhost:${PORT}${server.graphqlPath}`
+    );
+  } catch (error) {
+    console.log(error);
+    console.log(
+      "⛔ There was an error starting the server. See above for details."
+    );
+    console.log("Shutting down.");
+    process.exit(1);
+  }
 }
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-// Create a new instance of an Apollo server with the GraphQL schema
-const startApolloServer = async (typeDefs, resolvers) => {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-  });
-  
-  await server.start();
-  server.applyMiddleware({ app });
-  
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    })
-  })
-};
-
-// Call the async function to start the server
-startApolloServer(typeDefs, resolvers);
-  
